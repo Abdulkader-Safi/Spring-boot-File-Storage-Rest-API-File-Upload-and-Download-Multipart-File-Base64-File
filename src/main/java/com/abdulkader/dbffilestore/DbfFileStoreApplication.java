5a063c5dbda04afec12333cb8350cad795ad0ac4
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-08-14 23:42:38 +0300

    initalize Spring-boot-File-Storage-Rest-API-File-Upload-and-Download-Multipart-File-Base64-File

---
+++ src/main/java/com/abdulkader/dbffilestore/DbfFileStoreApplication.java
@@ -0,0 +1,13 @@
+package com.abdulkader.dbffilestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbfFileStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbfFileStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abdulkader/dbffilestore/DbfFileStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdulkader.dbffilestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbfFileStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
